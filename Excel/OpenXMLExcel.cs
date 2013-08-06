ea98ac6d79dc75eddc421b85c86bbea75f8a9251
Henry Hu <henry.hyh@gmail.com>
2013-08-06 10:46:17 +0800

    初始代码

---
+++ Excel/OpenXMLExcel.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace Excel
+{
+    public class OpenXMLExcel : IDisposable
+    {
+        #region Constrution and dispose
+
+        SpreadsheetDocument spreadSheet;
+
+        public WorksheetPart CurrentWorksheetPart { get; set; }
+
+        SharedStringTablePart shareStringPart;
+
+        public OpenXMLExcel(Stream stream)
+        {
+            spreadSheet = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook);
+            WorkbookPart workbookPart = spreadSheet.AddWorkbookPart();
+            workbookPart.Workbook = new Workbook();
+
+            WorksheetPart worksheetPart = InsertWorksheet(spreadSheet.WorkbookPart);
+
+            if (spreadSheet.WorkbookPart.GetPartsOfType<SharedStringTablePart>().Count() > 0)
+            {
+                shareStringPart = spreadSheet.WorkbookPart.GetPartsOfType<SharedStringTablePart>().First();
+            }
+            else
+            {
+                shareStringPart = spreadSheet.WorkbookPart.AddNewPart<SharedStringTablePart>();
+            }
+
+            shareStringPart.SharedStringTable = new SharedStringTable();
+            shareStringPart.SharedStringTable.Count = 1;
+            shareStringPart.SharedStringTable.UniqueCount = 1;
+
+            CurrentWorksheetPart = worksheetPart;
+        }
+
+        public void Dispose()
+        {
+            spreadSheet.Close();
+            spreadSheet.Dispose();
+        }
+
+        #endregion
+
+        #region Public interface
+
+        public void WriteDataIntoWorkSheet(int startx, int starty, DataTable dt)
+        {
+            if (startx < 1) startx = 1;
+            if (starty < 1) starty = 1;
+
+            WorksheetPart worksheetPart = CurrentWorksheetPart;
+            starty -= 1;
+            int j = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                j++;
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    string name = GetColumnName(i + starty);
+                    string text = Convert.IsDBNull(dr[i]) ? "" : dr[i].ToString();
+                    int index = InsertSharedStringItem(text, shareStringPart);
+                    Cell cell = InsertCellInWorksheet(name, Convert.ToUInt32(j + startx), worksheetPart);
+
+                    cell.CellValue = new CellValue(index.ToString());
+                    cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+                    worksheetPart.Worksheet.Save();
+                }
+            }
+        }
+
+        public void WriteDataIntoWorkSheet(int startx, int starty, string[][] data)
+        {
+            if (startx < 1) startx = 1;
+            if (starty < 1) starty = 1;
+
+            WorksheetPart worksheetPart = CurrentWorksheetPart;
+            starty -= 1;
+            int i = 0;
+            foreach (string[] row in data)
+            {
+                int j = 0;
+                foreach (string text in row)
+                {
+                    string name = GetColumnName(j + starty);
+                    int index = InsertSharedStringItem(text, shareStringPart);
+                    Cell cell = InsertCellInWorksheet(name, Convert.ToUInt32(i + startx), worksheetPart);
+
+                    cell.CellValue = new CellValue(index.ToString());
+                    cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+                    worksheetPart.Worksheet.Save();
+                    j++;
+                }
+                i++;
+            }
+        }
+
+        public void RenameCurrentWorksheet(string sheetName)
+        {
+            WorkbookPart workbookPart = spreadSheet.WorkbookPart;
+
+            Sheets sheets = workbookPart.Workbook.GetFirstChild<Sheets>();
+            Sheet sheet = sheets.Elements<Sheet>().Where(x => x.Id == workbookPart.GetIdOfPart(CurrentWorksheetPart)).FirstOrDefault();
+            if (null != sheet)
+            {
+                sheet.Name = sheetName;
+            }
+        }
+
+        public void AddNewWorksheet(string sheetName = "")
+        {
+            WorkbookPart workbookPart = spreadSheet.WorkbookPart;
+
+            WorksheetPart newWorksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+            newWorksheetPart.Worksheet.Save();
+            CurrentWorksheetPart = newWorksheetPart;
+            string relationshipId = workbookPart.GetIdOfPart(newWorksheetPart);
+            Sheets sheets = workbookPart.Workbook.GetFirstChild<Sheets>();
+            uint sheetId = 1;
+            if (sheets.Elements<Sheet>().Count() > 0)
+            {
+                sheetId = sheets.Elements<Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+            }
+            if (string.IsNullOrWhiteSpace(sheetName))
+            {
+                sheetName = "Sheet" + sheetId;
+            }
+
+            // Append the new worksheet and associate it with the workbook.
+            Sheet sheet = new Sheet() { Id = relationshipId, SheetId = sheetId, Name = sheetName };
+            sheets.Append(sheet);
+            workbookPart.Workbook.Save();
+        }
+
+        #endregion
+
+        #region private static OpenXml methods
+
+        private static int InsertSharedStringItem(string text, SharedStringTablePart shareStringPart)
+        {
+            // If the part does not contain a SharedStringTable, create one.
+            if (shareStringPart.SharedStringTable == null)
+            {
+                shareStringPart.SharedStringTable = new SharedStringTable();
+                shareStringPart.SharedStringTable.Count = 1;
+                shareStringPart.SharedStringTable.UniqueCount = 1;
+            }
+            int i = 0;
+            // Iterate through all the items in the SharedStringTable. If the text already exists, return its index.
+            foreach (SharedStringItem item in shareStringPart.SharedStringTable.Elements<SharedStringItem>())
+            {
+                if (item.InnerText == text)
+                {
+                    return i;
+                }
+                i++;
+            }
+
+            // The text does not exist in the part. Create the SharedStringItem and return its index.
+            shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(text)));
+            shareStringPart.SharedStringTable.Save();
+
+            return i;
+        }
+
+        private static Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+        {
+            Worksheet worksheet = worksheetPart.Worksheet;
+            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+            string cellReference = columnName + rowIndex;
+
+            // If the worksheet does not contain a row with the specified row index, insert one.
+            Row row;
+            if (sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).Count() != 0)
+            {
+                row = sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).First();
+            }
+            else
+            {
+                row = new Row() { RowIndex = rowIndex };
+                sheetData.Append(row);
+            }
+
+            // If there is not a cell with the specified column name, insert one.  
+            if (row.Elements<Cell>().Where(c => c.CellReference.Value == columnName + rowIndex).Count() > 0)
+            {
+                return row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
+            }
+            else
+            {
+                // Cells must be in sequential order according to CellReference. Determine where to insert the new cell.
+                Cell refCell = null;
+                foreach (Cell cell in row.Elements<Cell>())
+                {
+                    if (string.Compare(cell.CellReference.Value, cellReference, true) > 0)
+                    {
+                        refCell = cell;
+                        break;
+                    }
+                }
+
+                Cell newCell = new Cell() { CellReference = cellReference };
+                row.InsertBefore(newCell, refCell);
+
+                worksheet.Save();
+                return newCell;
+            }
+        }
+
+        private static WorksheetPart InsertWorksheet(WorkbookPart workbookPart)
+        {
+            // Add a new worksheet part to the workbook.
+            WorksheetPart newWorksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+            newWorksheetPart.Worksheet.Save();
+
+            workbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+
+            Sheets sheets = workbookPart.Workbook.GetFirstChild<Sheets>();
+            string relationshipId = workbookPart.GetIdOfPart(newWorksheetPart);
+
+            // Get a unique ID for the new sheet.
+            uint sheetId = 1;
+            if (sheets.Elements<Sheet>().Count() > 0)
+            {
+                sheetId = sheets.Elements<Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+            }
+
+            string sheetName = "Sheet" + sheetId;
+
+            // Append the new worksheet and associate it with the workbook.
+            Sheet sheet = new Sheet() { Id = relationshipId, SheetId = sheetId, Name = sheetName };
+            sheets.Append(sheet);
+
+            workbookPart.Workbook.Save();
+            return newWorksheetPart;
+        }
+
+        /// <summary>
+        /// Given a Worksheet and a cell name, verifies that the specified cell exists.
+        /// If it does not exist, creates a new cell. 
+        /// </summary>
+        /// <param name="worksheet"></param>
+        /// <param name="cellName"></param>
+        private static void CreateSpreadsheetCellIfNotExist(Worksheet worksheet, string cellName)
+        {
+            string columnName = GetColumnName(cellName);
+            uint rowIndex = GetRowIndex(cellName);
+
+            IEnumerable<Row> rows = worksheet.Descendants<Row>().Where(r => r.RowIndex.Value == rowIndex);
+
+            // If the Worksheet does not contain the specified row, create the specified row.
+            // Create the specified cell in that row, and insert the row into the Worksheet.
+            if (rows.Count() == 0)
+            {
+                Row row = new Row() { RowIndex = new UInt32Value(rowIndex) };
+                Cell cell = new Cell() { CellReference = new StringValue(cellName) };
+                row.Append(cell);
+                worksheet.Descendants<SheetData>().First().Append(row);
+                worksheet.Save();
+            }
+            else
+            {
+                Row row = rows.First();
+
+                IEnumerable<Cell> cells = row.Elements<Cell>().Where(c => c.CellReference.Value == cellName);
+
+                // If the row does not contain the specified cell, create the specified cell.
+                if (cells.Count() == 0)
+                {
+                    Cell cell = new Cell() { CellReference = new StringValue(cellName) };
+                    row.Append(cell);
+                    worksheet.Save();
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Given a cell name, parses the specified cell to get the column name.
+        /// </summary>
+        /// <param name="cellName"></param>
+        /// <returns></returns>
+        private static string GetColumnName(string cellName)
+        {
+            // Create a regular expression to match the column name portion of the cell name.
+            Regex regex = new Regex("[A-Za-z]+");
+            Match match = regex.Match(cellName);
+
+            return match.Value;
+        }
+
+        /// <summary>
+        /// Given a cell name, parses the specified cell to get the row index.
+        /// </summary>
+        /// <param name="cellName"></param>
+        /// <returns></returns>
+        private static uint GetRowIndex(string cellName)
+        {
+            // Create a regular expression to match the row index portion the cell name.
+            Regex regex = new Regex(@"\d+");
+            Match match = regex.Match(cellName);
+
+            return uint.Parse(match.Value);
+        }
+
+        #endregion
+
+        #region Utility methods
+
+        private static string GetColumnName(int index)
+        {
+            string name = "";
+            char[] columnNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            int num = index;
+            do
+            {
+                int i = num % 26;
+                name = columnNames[i] + name;
+                num = num / 26 - 1;
+            } while (num > -1);
+            if (string.IsNullOrEmpty(name))
+                name = "A";
+            return name;
+        }
+
+        #endregion
+    }
+}
